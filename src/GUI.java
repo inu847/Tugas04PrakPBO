b2cbc344d8c3b8462bd813863f5821f94e46acc0
Brilljack <91831790+Brilljack@users.noreply.github.com>
2021-10-19 21:01:15 +0700

    Error

--- src/GUI.java
+++ src/GUI.java
@@ -62,25 +62,22 @@ public class GUI extends JFrame{
                         result = keliling.persegiPanjang(pjg, lbr);
                     }
                 }else if(ops == "SEGITIGA SAMA SISI"){
-//                    Luas
-                    double als = Double.parseDouble(alas.getText());
-                    double tng = Double.parseDouble(tinggi.getText());
-//                    Keliling
                     double sis = Double.parseDouble(sisi.getText());
                     if (luasRadioButton.isSelected()){
+                        double als = Double.parseDouble(alas.getText());
+                        double tng = Double.parseDouble(tinggi.getText());
                         result = luas.segitiga(als, tng);
                     }else if(kelilingRadioButton.isSelected()){
+                        double sis = Double.parseDouble(sisi.getText());
                         result = keliling.segitigaSamaSisi(sis);
                     }
                 }else if(ops == "SEGITIGA SAMA KAKI"){
-//                    Luas
-                    double als = Double.parseDouble(alas.getText());
-                    double tng = Double.parseDouble(tinggi.getText());
-//                    Keliling
-                    double sis = Double.parseDouble(sisi.getText());
                     if (luasRadioButton.isSelected()){
+                        double als = Double.parseDouble(alas.getText());
+                        double tng = Double.parseDouble(tinggi.getText());
                         result = luas.segitiga(als, tng);
                     }else if(kelilingRadioButton.isSelected()){
+                        double sis = Double.parseDouble(sisi.getText());
                         result = keliling.segitigaSamaKaki(sis);
                     }
                 }
@@ -158,6 +155,9 @@ public class GUI extends JFrame{
                     sisi.setVisible(false);
                     masukanSisi.setVisible(false);
                 }else if(ops == "SEGITIGA SAMA SISI"){
+                    sisi.setEditable(false);
+                    sisi.setVisible(false);
+                    masukanSisi.setVisible(false);
                     jari.setEditable(false);
                     jari.setVisible(false);
                     masukanJari.setVisible(false);
@@ -168,6 +168,9 @@ public class GUI extends JFrame{
                     panjang.setVisible(false);
                     masukanPanjang.setVisible(false);
                 }else if(ops == "SEGITIGA SAMA KAKI"){
+                    sisi.setEditable(false);
+                    sisi.setVisible(false);
+                    masukanSisi.setVisible(false);
                     jari.setEditable(false);
                     jari.setVisible(false);
                     masukanJari.setVisible(false);
@@ -205,6 +208,72 @@ public class GUI extends JFrame{
                 masukanLebar.setVisible(false);
                 masukanPanjang.setVisible(false);
                 masukanAlas.setVisible(false);
+            }
+        });
+        kelilingRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String opsi = String.valueOf(comboBox1.getSelectedItem());
+                sisi.setEditable(true);
+                tinggi.setEditable(true);
+                panjang.setEditable(true);
+                lebar.setEditable(true);
+                alas.setEditable(true);
+                jari.setEditable(true);
+                sisi.setVisible(true);
+                tinggi.setVisible(true);
+                panjang.setVisible(true);
+                lebar.setVisible(true);
+                alas.setVisible(true);
+                jari.setVisible(true);
+                masukanSisi.setVisible(true);
+                masukanTinggi.setVisible(true);
+                masukanPanjang.setVisible(true);
+                masukanLebar.setVisible(true);
+                masukanAlas.setVisible(true);
+                masukanJari.setVisible(true);
+                if(opsi == "SEGITIGA SAMA SISI"){
+                    if(kelilingRadioButton.isSelected()){
+                        sisi.setEditable(true);
+                        sisi.setVisible(true);
+                        masukanSisi.setVisible(true);
+                        tinggi.setEditable(false);
+                        tinggi.setVisible(false);
+                        masukanTinggi.setVisible(false);
+                        alas.setEditable(false);
+                        alas.setVisible(false);
+                        masukanAlas.setVisible(false);
+                        jari.setEditable(false);
+                        jari.setVisible(false);
+                        masukanJari.setVisible(false);
+                        lebar.setEditable(false);
+                        lebar.setVisible(false);
+                        masukanLebar.setVisible(false);
+                        panjang.setEditable(false);
+                        panjang.setVisible(false);
+                        masukanPanjang.setVisible(false);
+                    }
+                    else if (luasRadioButton.isSelected()){
+                        tinggi.setEditable(true);
+                        tinggi.setEditable(true);
+                        masukanTinggi.setVisible(true);
+                        alas.setEditable(true);
+                        alas.setVisible(true);
+                        masukanAlas.setVisible(true);
+                        sisi.setEditable(false);
+                        sisi.setVisible(false);
+                        masukanSisi.setVisible(false);
+                        jari.setEditable(false);
+                        jari.setVisible(false);
+                        masukanJari.setVisible(false);
+                        lebar.setEditable(false);
+                        lebar.setVisible(false);
+                        masukanLebar.setVisible(false);
+                        panjang.setEditable(false);
+                        panjang.setVisible(false);
+                        masukanPanjang.setVisible(false);
+                    }
+                }
             }
         });
     }
